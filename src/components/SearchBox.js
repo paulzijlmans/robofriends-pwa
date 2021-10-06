a730c40850fde3bbc8fd00a540b68041610ad555
Paul Zijlmans <paul.zijlmans@devolksbank.nl>
2021-10-06 10:29:28 +0200

    added improvements

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const SearchBox = ({searchChange}) => {
+const SearchBox = ({ searchChange }) => {
   return (
     <div className='pa2'>
       <input
+        aria-label='Search Robots'
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
